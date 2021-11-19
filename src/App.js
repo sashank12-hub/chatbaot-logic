d03da0c34ba01e0c2dfcf9016bb83239a8a13a89
Chavaly Sai Sashank <chasaisa@publicisgroupe.net>
2021-11-19 15:19:37 +0530

    logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useReducer } from "react";
+
+import Questionhandler from "./function";
+import data from "./data.json";
 
 function App() {
+  const questions = data;
+  const initialformstate = {
+    uniqueid: 123,
+    userresponse: [],
+  };
+  const [text, settext] = useState("");
+  const [isText, setisText] = useState(false);
+  const [currentquestion, setcurrentquestion] = useState({});
+  const [counter, setcounter] = useState(0);
+  const [form, setform] = useState(initialformstate);
+  useEffect(() => {
+    setcurrentquestion(questions[0]);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  useEffect(() => {
+    setcurrentquestion(questions[counter]);
+    console.log(counter);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [counter]);
+
+  const handleSubmit = async (obj1, value) => {
+    console.log(obj1, value);
+    let uservalues = [...form.userresponse, { item: obj1, response: value }];
+
+    setform({ ...form, userresponse: uservalues });
+
+    if (counter >= questions.length - 1) {
+      window.localStorage.setItem(
+        "chatbotdata",
+        JSON.stringify({ ...form, userresponse: uservalues })
+      );
+      alert("thanks");
+    } else {
+      setcounter((counter) => counter + 1);
+    }
+  };
+  const handlebutton = () => {
+    handleSubmit(currentquestion, text);
+    settext("");
+  };
+  const textchangehandler = (e) => {
+    //  console.log(e.target.value);
+    settext(e.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Questionhandler(currentquestion, handleSubmit)}
+
+      <input
+        id={currentquestion.id}
+        value={text}
+        type="text"
+        onChange={(e) => {
+          textchangehandler(e);
+        }}
+        style={{ marginTop: "20px", marginLeft: "130px" }}
+        disabled={currentquestion.type !== "text"}
+      />
+      <button
+        onClick={() => handlebutton()}
+        disabled={currentquestion.type !== "text"}
+      >
+        send
+      </button>
     </div>
   );
 }
---
+++ src/function.js
@@ -0,0 +1,136 @@
+import React from "react";
+export default function Question(item, callback) {
+  var selected = [];
+  const reader = new FileReader();
+  const radiobuttonhandler = (e) => {
+    // console.log(e.target.value);
+    callback(item, e.target.value);
+  };
+  const checkboxhandler = (e, array) => {
+    array.forEach((fruite) => {
+      if (fruite.value === e.target.value) fruite.ischecked = e.target.checked;
+    });
+    selected = [...array];
+  };
+  const selecthandler = (e) => {
+    callback(item, e.target.value);
+    console.log(e.target.value);
+  };
+  const filechangehandler = (item, event) => {
+    console.log("here", event.target.files[0]);
+
+    callback(item, event.target.files[0].name);
+  };
+  let htmlElement;
+  switch (item.type) {
+    case "select":
+      htmlElement = (
+        <div>
+          <label htmlFor={item.id}>{item.question}</label>
+          {item.options.map((option, index) => {
+            return (
+              <div key={index}>
+                <label htmlFor={option}>{option}</label>
+                <input
+                  type="radio"
+                  name="radiobutton"
+                  value={option}
+                  id={option}
+                  onChange={(e) => radiobuttonhandler(e)}
+                />
+              </div>
+            );
+          })}
+        </div>
+      );
+      break;
+    case "multi":
+      let array = item.options.map((item) => {
+        return {
+          value: item,
+          ischecked: false,
+        };
+      });
+
+      htmlElement = (
+        <div>
+          <label style={{ margin: "10px" }} htmlFor={item.id}>
+            {item.question}
+          </label>
+          {array.map((option, index) => {
+            return (
+              <div key={index}>
+                <input
+                  type="checkbox"
+                  name="checkbox"
+                  value={option.value}
+                  id={option.value}
+                  onChange={(event) => checkboxhandler(event, array)}
+                />
+                <label htmlFor={option.value}>{option.value}</label>
+              </div>
+            );
+          })}
+          <button
+            onClick={() => {
+              let selectedcheckboxes = selected.filter(
+                (item) => item.ischecked === true
+              );
+              callback(item, [...selectedcheckboxes.map((item) => item.value)]);
+            }}
+          >
+            submit response
+          </button>
+        </div>
+      );
+      break;
+    case "text":
+      htmlElement = (
+        <div>
+          <label htmlFor={item.id}>{item.question}</label>
+        </div>
+      );
+      break;
+    case "dropdown":
+      htmlElement = (
+        <div>
+          <label htmlFor="dropdown">{item.question}</label>
+
+          <select
+            name="dropdown"
+            id="dropdown"
+            onChange={(e) => selecthandler(e)}
+          >
+            <option>select option from list</option>
+            {item.options.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </div>
+      );
+      break;
+    case "file":
+      htmlElement = (
+        <div>
+          <label htmlFor="myfile">Select a file:</label>
+
+          <input
+            type="file"
+            id="myfile"
+            name="myfile"
+            accept="image/*"
+            onChange={(e) => {
+              console.log(e.target.files[0]);
+              filechangehandler(item, e);
+            }}
+          />
+        </div>
+      );
+      break;
+    default:
+      return htmlElement;
+  }
+  return htmlElement;
+}

4f85aed6d73d73e0a0adf7932e7181c26739d8e5
Raj26-dev <92971032+Raj26-dev@users.noreply.github.com>
2021-12-22 11:46:19 +0530

    Update fhdfd.js

--- src/fhdfd.js
+++ src/fhdfd.js
@@ -313,3 +313,50 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+// import React, { useState, useEffect, useRef } from "react";
+// import ReactDOM from "react-dom";
+// import {v1 as uuid} from "uuid";
+// import "./styles.css";
+// //  import Message from "./Message";
+
+// const Message = ({ messages }) => {
+//   const messagesEndRef = useRef(null);
+//   const scrollToBottom = () => {
+//     messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+//   };
+//   useEffect(scrollToBottom, [messages]);
+
+//   return (
+//     <div className="messagesWrapper">
+//       {messages.map(message => (
+//         <span key={message}>{message}</span>
+//       ))}
+//       <div ref={messagesEndRef} />
+//     </div>
+//   );
+// };
+
+
+// const Imgs=()=> {
+//   const [messages, setMessages] = useState([]);
+//   const addMessages = () => {
+//     setMessages(m => [...m, uuid()]);
+//   };
+//   return (
+//     <div className="App">
+//       <button className="addButton" onClick={addMessages}>
+//         Add message
+//       </button>
+//       <Message messages={messages} ></Message>
+//     </div>
+//   );
+// }
+// export default Imgs
+
+
+
